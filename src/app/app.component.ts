f04b44d618d97da23bca1052fbc25b4d01a3429d
Paburitel <nik13nik82@gmail.com>
2023-06-07 15:40:43 +0200

    styles and base component structure have been fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,37 +1,39 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, ChangeDetectorRef, Component} from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
-interface Message {
-  question: string;
-  answer: string;
-}
+import { Message } from "./chat-client/chat-client.component";
+import {delay, of} from "rxjs";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  inputText: string = '';
   loading: boolean = false;
   messages: Message[] = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private cdr: ChangeDetectorRef) {}
 
-  submitRequest() {
-    if (this.inputText) {
-      this.loading = true;
-
-      const url = `http://localhost:8000/answer?query=${encodeURIComponent(this.inputText)}`;
-      this.http.get(url).subscribe((data: any) => {
-        const message: Message = {
-          question: this.inputText,
-          answer: data.answer
-        };
-        this.messages.push(message);
-        this.loading = false;
-        this.inputText = '';
-      });
+  public submitRequest(inputText: string): void {
+    if (!inputText) {
+      return;
     }
+    this.loading = true;
+
+    const url = `http://localhost:8000/answer?query=${encodeURIComponent(inputText)}`;
+
+    // of({answer: 'I\'m sorry, I don\'t have enough context to answer your question accurately. Could you please provide me with more information about the situation?'})
+    //   .pipe(delay(1500))
+    this.http.get(url)
+      .subscribe((data: any) => {
+      const message: Message = {
+        question: inputText,
+        answer: data.answer
+      };
+      this.messages = [...this.messages, message];
+      this.loading = false;
+      this.cdr.detectChanges(); // should be deleted after HTTP service implementation
+    });
   }
 }
